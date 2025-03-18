351bbf33df3721e620e40d91aca6f386f67105c1
kisnikser <nekitbrain@yandex.ru>
2025-03-18 11:05:20 +0300

    upd HyperOptimizer interface

--- src/hippotrainer/direct.py
+++ src/hippotrainer/direct.py
@@ -6,9 +6,9 @@ class Direct(HyperOptimizer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def hyper_grad(self, val_loss, hyperparam):
+    def hyper_grad(self, train_loss, val_loss, hyperparam):
         val_loss.backward(retain_graph=True)
-        grad = hyperparam.grad.clone()
+        hyper_grad = hyperparam.grad.clone()
         self.zero_grad()
         self.zero_hyper_grad()
-        return grad
+        return hyper_grad
--- src/hippotrainer/hyper_optimizer.py
+++ src/hippotrainer/hyper_optimizer.py
@@ -1,16 +1,32 @@
 import torch
+import torch.nn as nn
+from torch.optim import Optimizer
+from collections.abc import Iterable
+from typing import Any, Union
+
+torch.autograd.set_detect_anomaly(True)
 
 
 class HyperOptimizer:
-    def __init__(self, optimizer, model, criterion, val_loader, T, hyper_lr, hyperparams):
-        self.optimizer = optimizer
-        self.model = model
-        self.criterion = criterion
-        self.val_loader = val_loader
-        self.T = T
-        self.step_count = 0
-        self.hyper_lr = hyper_lr
+    def __init__(
+        self,
+        hyperparams: dict[str, torch.Tensor],
+        hyper_lr: Union[float, torch.Tensor] = 1e-3,
+        inner_steps: int = 1,
+        model: nn.Module = None,
+        optimizer: Optimizer = None,
+        val_loader: Iterable[Any, Any] = None,
+        criterion: nn.Module = None,
+    ):
         self.hyperparams = hyperparams
+        self.hyper_lr = hyper_lr
+        self.inner_steps = inner_steps
+        self.model = model
+        self.optimizer = optimizer
+        self.val_loader = val_loader
+        self.criterion = criterion
+
+        self.step_count = 0
         self._hyperparams_requires_grad_(False)
 
     def _hyperparams_requires_grad_(self, requires_grad: bool = True):
@@ -34,19 +50,19 @@ class HyperOptimizer:
         val_loss /= len(self.val_loader)
         return val_loss
 
-    def hyper_grad(self, val_loss, hyperparam):
+    def hyper_grad(self, train_loss, val_loss: torch.Tensor, hyperparam: torch.Tensor):
         raise NotImplementedError
 
-    def step(self):
+    def step(self, train_loss):
         self.step_count += 1
         self.optimizer.step()
-        if self.step_count % self.T == 0:
+        if self.step_count % self.inner_steps == 0:
             self._hyperparams_requires_grad_(True)
-            self.hyper_step()
+            self.hyper_step(train_loss)
             self._hyperparams_requires_grad_(False)
 
-    def hyper_step(self):
+    def hyper_step(self, train_loss):
         val_loss = self.evaluate()
         for name, hyperparam in self.hyperparams.items():
-            hyper_grad = self.hyper_grad(val_loss, hyperparam)
+            hyper_grad = self.hyper_grad(train_loss, val_loss, hyperparam)
             hyperparam.data -= self.hyper_lr * hyper_grad
